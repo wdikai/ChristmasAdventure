7f3221a721b479b3355ed35811f549bba9a1e9e4
Danil <danilstulbo@outlook.com>
2015-12-25 11:39:06 +0200

    added Demon

--- ChristmasAdventure/www/scripts/App.js
+++ ChristmasAdventure/www/scripts/App.js
@@ -37,7 +37,7 @@ function StartGame() {
     document.getElementById('PauseGame').style.display = 'none';
     document.getElementById('control-block').style.display = 'block';
     $("#canvas").show();
-init();
+    init();
 }
 function init() {
     play = true;
@@ -45,28 +45,29 @@ function init() {
     background = new GameObject("Background", 1366, canvas.height, 0, 0, 'images/backgrounds/Background.jpg', 7);
     background.update = function () {
         if (background.x < -background.width)
-		background.x = background2.x + background2.width;
-		background.x -= background.speed;
-	}
+            background.x = background2.x + background2.width;
+        background.x -= background.speed;
+    }
     background2 = new GameObject("Background", background.width, canvas.height, background.width, 0, 'images/backgrounds/Background.jpg', background.speed);
     background2.update = function () {
         if (background2.x < -background.width)
-		background2.x = background.x + background.width;
-		background2.x -= background2.speed;
-	}
+            background2.x = background.x + background.width;
+        background2.x -= background2.speed;
+    }
 
-	lastTime = Date.now();
-	player = new Player(100, 100, 100, canvas.height / 2, 'images/entities/Player.png', 3);
-	GameEnd = false;
-	GameObjects = [];
-	pastTime = 0;
-	Icetemp = 0;
-	Horsetemp = 0;
-	Sputniktemp = 0;
+    lastTime = Date.now();
+    player = new Player(100, 100, 100, canvas.height / 2, 'images/entities/Player.png', 3);
+    GameEnd = false;
+    GameObjects = [];
+    pastTime = 0;
+    Icetemp = 0;
+    Horsetemp = 0;
+    ghostTemp = 0;
+    demonTemp = 0;
     Lifetemp = 0;
-	TempCollision = 0;
+    TempCollision = 0;
     panel = new Panel();
-	ctx.font = 'bold 50px courier';
+    ctx.font = 'bold 50px courier';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'top';
     ctx.fillStyle = '#ccc';
@@ -74,27 +75,28 @@ function init() {
         [
             'images/backgrounds/Background.jpg',
             'images/entities/Player.png',
-            'images/entities/Ice.png', 
-            'images/entities/FrameHorse.png', 
-            'images/entities/Balls.png', 
-            'images/entities/Monster.png', 
-            'images/ui/Life.png', 
+            'images/entities/Ice.png',
+            'images/entities/FrameHorse.png',
+            'images/entities/Balls.png',
+            'images/entities/Monster.png',
+            'images/entities/Demon.png',
+            'images/ui/Life.png',
             'images/Iceimg.png'
         ]);
-	resources.onReady(main);
-	}
+    resources.onReady(main);
+}
 var lastTime;
 function main() {
     if (play == true && GameEnd == false) {
-    var now = Date.now();
-    var dt = (now - lastTime) / 1000.0;
+        var now = Date.now();
+        var dt = (now - lastTime) / 1000.0;
         pastTime += dt;
-	AdditionEntities(pastTime);
-    update(dt);
-    render(ctx);
-    checkCollisions(pastTime);
-    RemoveEntity();
-    lastTime = now;
+        AdditionEntities(pastTime);
+        update(dt);
+        render(ctx);
+        checkCollisions(pastTime);
+        RemoveEntity();
+        lastTime = now;
     }
     requestAnimFrame(main);
 };
@@ -103,15 +105,15 @@ function render(ctx) {
     ctx.drawImage(resources.get('images/backgrounds/Background.jpg'), background2.x, background2.y, background2.width, background2.height);
     DrawLife();
     DrawCookies();
-	DrawEntity(ctx);
+    DrawEntity(ctx);
     if (TempCollision > pastTime) {
         if (light) {
-        player.sprite.render(ctx);
+            player.sprite.render(ctx);
             light = !light;
-    }
+        }
         else {
-        light = !light;
-    }
+            light = !light;
+        }
     }
     else {
         player.sprite.render(ctx);
@@ -120,20 +122,25 @@ function render(ctx) {
 
 }
 function update(dt) {
-	background.update();
-	background2.update();
-	player.sprite.update(dt);
+    background.update();
+    background2.update();
+    player.sprite.update(dt);
     for (i = 0; i < GameObjects.length; i++)
-	    if (GameObjects[i].type != "Monster")
-	GameObjects[i].update(dt);
-    else
+        if (GameObjects[i].type == "Monster") {
             GameObjects[i].update(dt, 'sin');
+        }
+        else if (GameObjects[i].type == "Demon") {
+            GameObjects[i].update(dt, 'cos');
+        } else {
+            GameObjects[i].update(dt);
+        }
+
 }
 
 function rand(min, max) {
     var rand = min - 0.5 + Math.random() * (max - min + 1)
-	rand = Math.round(rand);
-	return rand;
+    rand = Math.round(rand);
+    return rand;
 }
 
 document.getElementById("shot").addEventListener("touchstart", function (e) {
@@ -161,24 +168,32 @@ document.onkeydown = function (e) {
 }
 function DrawEntity(ctx) {
     for (i = 0; i < GameObjects.length; i++)
-		GameObjects[i].draw(ctx);
+        GameObjects[i].draw(ctx);
 }
 
 function AdditionEntities(pastTime) {
     if (pastTime > Icetemp) {
-	    GameObjects.push(new GameObject("Ice", 48, 48, rand(canvas.width + 30, canvas.width + background.width), rand(50, canvas.height - 48), 'images/entities/Ice.png', rand(8, 15)));
+        GameObjects.push(new GameObject("Ice", 48, 48, rand(canvas.width + 30, canvas.width + background.width), rand(50, canvas.height - 48), 'images/entities/Ice.png', rand(8, 15)));
         Icetemp = pastTime + 1;
-	}
-    if (parseInt(pastTime, 10) > Sputniktemp) {
+    }
+    if (parseInt(pastTime, 10) > ghostTemp) {
         for (i = 0; i < rand(1, 4) ; i++)
-            GameObjects.push(new GameObject("Monster", 100, 100, rand(canvas.width + 30, 10000), rand(50, canvas.height - 200), 'images/entities/Monster.png', rand(10, 20)));
-        Sputniktemp = parseInt(pastTime, 10) + rand(4, 8);
-	}
+            GameObjects.push(new GameObject("Monster", 100, 100, rand(canvas.width + 30, 10000), rand(50, canvas.height - 200), 'images/entities/Monster.png', rand(15, 20)));
+        ghostTemp = parseInt(pastTime, 10) + rand(15, 20);
+    }
+
     if (parseInt(pastTime, 10) > Horsetemp) {
         for (i = 0; i < rand(1, 20) ; i++)
-            GameObjects.push(new GameObject("Horse", 167, 100, rand(canvas.width + 30, 10000), rand(50, canvas.height - 56), 'images/entities/FrameHorse.png', rand(10, 20)));
+            GameObjects.push(new GameObject("Horse", 167, 100, rand(canvas.width + 30, 10000), rand(50, canvas.height - 56), 'images/entities/FrameHorse.png', rand(5, 10)));
         Horsetemp = parseInt(pastTime, 10) + rand(4, 8);
-	}
+    }
+
+    if (parseInt(pastTime, 10) > demonTemp) {
+        for (i = 0; i < rand(1, 20) ; i++)
+            GameObjects.push(new GameObject("Demon", 100, 100, rand(canvas.width + 30, 10000), rand(50, canvas.height - 56), 'images/entities/Demon.png', rand(10, 15)));
+        demonTemp = parseInt(pastTime, 10) + rand(15, 20);
+    }
+
     if (parseInt(pastTime, 10) > Lifetemp) {
         life = new GameObject("Life", 55, 55, rand(canvas.width + 30, canvas.width + background.width), rand(50, canvas.height - 56), 'images/ui/Life.png', 10);
         life.draw = function (ctx) {
@@ -193,7 +208,7 @@ function checkCollisions(pastTime) {
     if (TempCollision < parseInt(pastTime, 10)) {
         for (i = 0; i < GameObjects.length; i++) {
             if (player.collision(GameObjects[i]) && GameObjects[i].type != "Ice" && GameObjects[i].type != "Life" && GameObjects[i].type != "Ball") {
-                if(GameObjects[i].type === "Horse" && play && !GameEnd){
+                if (GameObjects[i].type === "Horse" && play && !GameEnd) {
                     SoundManager.play("Horse");
                 }
                 player.life--;
@@ -284,29 +299,29 @@ function GameOver() {
     var score = (pastTime * 5.5).toFixed();
     TimeScore.innerHTML = '<h1>Результат: ' + score + '</h1>';
     var lastScore = parseInt(loadScore());
-    if(lastScore < score){
+    if (lastScore < score) {
         saveScore(score);
     }
 
     $("#record").text(loadScore());
     document.getElementById('play-again').addEventListener('click', function () {
-            reset();
-        });
-    }
+        reset();
+    });
+}
 
 function reset() {
-        document.getElementById('game-over').style.display = 'none';
-        StartGame();
-    }
+    document.getElementById('game-over').style.display = 'none';
+    StartGame();
+}
 function MainMenu() {
-        $('#game-over').hide();
-        $('#PauseGame').hide();
-        $('#info').hide();
-        $('#achivment-list').hide();
-        $('#Menu').show();
-        play = false;
-        SoundManager.play("Main");
-    }
+    $('#game-over').hide();
+    $('#PauseGame').hide();
+    $('#info').hide();
+    $('#achivment-list').hide();
+    $('#Menu').show();
+    play = false;
+    SoundManager.play("Main");
+}
 
 function PauseGame() {
     play = !play;
@@ -324,7 +339,7 @@ document.getElementById("swipe").addEventListener("touchmove", function (event) 
 
         if (touches[0].pageY < canvas.height - player.height / 3 && touches[0].pageY > 50) {
             player.sprite.entity_pos[1] = touches[0].pageY - player.height / 2;
+        }
     }
 }
-    }
 );
